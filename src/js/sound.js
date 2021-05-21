b37d0ebd39d99316de5a1009b3ab1541cd7e3c70
Laksito Pamilih <laksito@pm.me>
2021-05-21 09:23:49 +0700

    stop last osc before starting new one

--- src/js/sound.js
+++ src/js/sound.js
@@ -2,20 +2,26 @@ class Sound {
   _startContext() {
     if (!this.context) {
       this.context = new (window.AudioContext || window.webkitAudioContext)();
+      this.gain = this.context.createGain();
+      this.gain.connect(this.context.destination)
     }
   }
 
   play(freq) {
     this._startContext();
-
+    console.log(this.context)
     const osc = this.context.createOscillator();
     osc.type = 'triangle';
     
-    osc.connect(this.context.destination);
+    osc.connect(this.gain);
     osc.frequency.setValueAtTime(freq, this.context.currentTime)
 
+    if (this.prevOsc) this.prevOsc.stop();
+    
     osc.start(this.context.currentTime);
     osc.stop(this.context.currentTime + 0.6);
+
+    this.prevOsc = osc;
   }
 }
 
